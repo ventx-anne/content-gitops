dec6d5a22978588f0ca851b97d77725cb2e6ca04
Anne Michael <127721751+ventx-anne@users.noreply.github.com>
2024-02-07 13:04:14 +0100

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hallo Version 1.1!"
+    return "Hallo Version 1.2!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
